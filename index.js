e63f08ef00824ccc6dcd32560f8121620811de85
Izel Nakri <contact@izelnakri.com>
2017-05-15 12:42:33 +0200

    styling changes

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-var fs = require('fs');
-var path = require('path');
-var mkdirp = require('mkdirp');
-var Writer = require('broccoli-caching-writer');
-var helpers = require('broccoli-kitchen-sink-helpers');
-var walkSync = require('walk-sync');
-var mapSeries = require('promise-map-series');
-var separator = require('postcss-separator');
+const fs = require('fs');
+const path = require('path');
+const mkdirp = require('mkdirp');
+const Writer = require('broccoli-caching-writer');
+const helpers = require('broccoli-kitchen-sink-helpers');
+const walkSync = require('walk-sync');
+const mapSeries = require('promise-map-series');
+const separator = require('postcss-separator');
 
-module.exports = Separator;
 Separator.prototype = Object.create(Writer.prototype);
 Separator.prototype.constructor = Separator;
-
-function Separator(inputTree, options) {
-	if (!(this instanceof Separator)) {
-		return new Separator(inputTree, options);
-	}
-	this.inputTree = inputTree;
-	this.options = options || {};
-};
-
 Separator.prototype.updateCache = function(srcDir, destDir) {
-	var self = this;
-	var paths = walkSync(srcDir);
+	// var self = this;
+	const paths = walkSync(srcDir);
 
-	return mapSeries(paths, function(relativePath) {
+	return mapSeries(paths, (relativePath) => {
 		if (/\/$/.test(relativePath)) {
-			mkdirp.sync(destDir + '/' + relativePath);
+			mkdirp.sync(`${destDir}/${relativePath}`);
 		} else {
 			helpers.copyPreserveSync(path.join(srcDir, relativePath), path.join(destDir, relativePath))
 
 			if (/\.css$/.test(relativePath)) {
-				var srcPath = path.join(srcDir, relativePath);
-				var destPath = path.join(destDir, relativePath);
+				const srcPath = path.join(srcDir, relativePath);
+				const destPath = path.join(destDir, relativePath);
 
-				var rawcss = fs.readFileSync(srcPath, {encoding: 'utf8'});
-				var data = separator.separate(rawcss, {dataFile: true});
-				var original = separator.separate(rawcss, {dataFile: false});
+				const rawcss = fs.readFileSync(srcPath, { encoding: 'utf8' });
+				const data = separator.separate(rawcss, { dataFile: true });
+				const original = separator.separate(rawcss, { dataFile: false });
 
-				// write files overwriting originals + data (if there is any data)
-				if(data.css) {
-					fs.writeFileSync(destPath, original.css, { encoding: 'utf8'});
-					var dataDestPath = destPath.replace(/\.css$/, '-data' + '.css');
-					fs.writeFileSync(dataDestPath, data.css, { encoding: 'utf8'});
+				if (data.css) { // write files overwriting originals + data (if there is any data)
+					fs.writeFileSync(destPath, original.css, { encoding: 'utf8' });
+					const dataDestPath = destPath.replace(/\.css$/, '-data.css');
+					fs.writeFileSync(dataDestPath, data.css, { encoding: 'utf8' });
 				}
 			}
 		}
 	});
 }
+
+function Separator(inputTree, options) {
+	if (!(this instanceof Separator)) {
+		return new Separator(inputTree, options);
+	}
+
+	this.inputTree = inputTree;
+	this.options = options || {};
+};
+
+module.exports = Separator;
