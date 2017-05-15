02f0a63377bcd91b1f628ddf88d1e87f1cd0d8a7
Izel Nakri <contact@izelnakri.com>
2017-05-15 14:50:45 +0200

    final version

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp');
 const Writer = require('broccoli-caching-writer');
-const helpers = require('broccoli-kitchen-sink-helpers');
+// const helpers = require('broccoli-kitchen-sink-helpers');
 const walkSync = require('walk-sync'); // maybe remove
-const mapSeries = require('promise-map-series'); // remove in future
+// const mapSeries = require('promise-map-series'); // remove in future
 const separator = require('postcss-separator');
 
 Separator.prototype = Object.create(Writer.prototype);
@@ -12,28 +12,24 @@ Separator.prototype.constructor = Separator;
 Separator.prototype.build = function() {
 	const srcDir = this.inputPaths[0];
 	const destDir = this.outputPath;
-
 	const paths = walkSync(srcDir);
 
-	return mapSeries(paths, (relativePath) => {
+	return paths.forEach((relativePath) => {
 		if (/\/$/.test(relativePath)) {
 			mkdirp.sync(`${destDir}/${relativePath}`);
-		} else {
-			helpers.copyPreserveSync(path.join(srcDir, relativePath), path.join(destDir, relativePath))
+		} else if (/\.css$/.test(relativePath)) {
+			const srcPath = path.join(srcDir, relativePath);
+			const rawcss = fs.readFileSync(srcPath, { encoding: 'utf8' });
 
-			if (/\.css$/.test(relativePath)) {
-				const srcPath = path.join(srcDir, relativePath);
+			const data = separator.separate(rawcss, { dataFile: true });
+			const original = separator.separate(rawcss, { dataFile: false });
+
+			if (data.css) { // write files overwriting originals + data (if there is any data)
 				const destPath = path.join(destDir, relativePath);
-
-				const rawcss = fs.readFileSync(srcPath, { encoding: 'utf8' });
-				const data = separator.separate(rawcss, { dataFile: true });
-				const original = separator.separate(rawcss, { dataFile: false });
-
-				// if (data.css) { // write files overwriting originals + data (if there is any data)
-				fs.writeFileSync(destPath, original.css, { encoding: 'utf8' });
 				const dataDestPath = destPath.replace(/\.css$/, '-data.css');
+
+				fs.writeFile(destPath, original.css, { encoding: 'utf8' });
 				fs.writeFileSync(dataDestPath, data.css, { encoding: 'utf8' });
-				// }
 			}
 		}
 	});
